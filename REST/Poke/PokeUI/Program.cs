a764d4f500d577a497480ebf425a7addbb6ba930
Kira <kirosa1994@gmail.com>
2022-02-18 11:13:21 -0500

    Submitted Factory Design Pattern

--- REST/Poke/PokeUI/Program.cs
+++ REST/Poke/PokeUI/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // using PokeModel;
 global using Serilog;
-using Microsoft.Extensions.Configuration;
 using PokeBL;
 using PokeDL;
 using PokeUI;
@@ -13,50 +12,54 @@ using PokeUI;
 //Creating and configuring our logger
 Log.Logger = new LoggerConfiguration().WriteTo.File("./logs/user.txt").CreateLogger();
 
-//Reading and obtaining connectionString from appsettings.json
-var configuration = new ConfigurationBuilder()
-                        .SetBasePath(Directory.GetCurrentDirectory())
-                        .AddJsonFile("appsettings.json")
-                        .Build();
 
-string _connectionString = configuration.GetConnectionString("Reference2DB");
 
 bool repeat = true;
 IMenu menu = new MainMenu();
+FactoryMenu factory = new FactoryMenu();
 
 while (repeat)
 {
   Console.Clear();
   menu.Display();
   MenuType ans = menu.UserChoice();
-
-  switch (ans)
+  if (ans != MenuType.Exit)
   {
-    case MenuType.AddPoke:
-      Log.Information("Displaying AddPokemon Menu to user");
-      menu = new AddPokeMenu(new PokemonBL(new SQLRepository(_connectionString)));
-      break;
-    case MenuType.SearchPokemon:
-      Log.Information("Displaying SearchPokemon Menu to user");
-      menu = new SearchPokemonMenu(new PokemonBL(new SQLRepository(_connectionString)));
-      break;
-    case MenuType.GetPokeAbility:
-      Log.Information("Displaying GetPokeAbility Menu to user");
-      menu = new GetPokeAbility(new PokemonBL(new SQLRepository(_connectionString)));
-      break;
-    case MenuType.MainMenu:
-      Log.Information("Displaying MainMenu to user");
-      menu = new MainMenu();
-      break;
-    case MenuType.Exit:
-      Log.Information("Exiting application");
-      Log.CloseAndFlush(); //To close our logger resource
-      repeat = false;
-      break;
-    default:
-      Console.WriteLine("Page does not exist!");
-      Console.WriteLine("Please press Enter to continue");
-      Console.ReadLine();
-      break;
+    menu = factory.CreateMenu(ans);
   }
+  else
+  {
+    repeat = false;
+  }
+
+  // switch (ans)
+  // {
+  //   case MenuType.AddPoke:
+  //     Log.Information("Displaying AddPokemon Menu to user");
+  //     menu = new AddPokeMenu(new PokemonBL(new SQLRepository(_connectionString)));
+  //     break;
+  //   case MenuType.SearchPokemon:
+  //     Log.Information("Displaying SearchPokemon Menu to user");
+  //     menu = new SearchPokemonMenu(new PokemonBL(new SQLRepository(_connectionString)));
+  //     break;
+  //   case MenuType.GetPokeAbility:
+  //     Log.Information("Displaying GetPokeAbility Menu to user");
+  //     menu = new GetPokeAbility(new PokemonBL(new SQLRepository(_connectionString)));
+  //     break;
+  //   case MenuType.MainMenu:
+  //     Log.Information("Displaying MainMenu to user");
+  //     menu = new MainMenu();
+  //     break;
+  //   case MenuType.Exit:
+  //     Log.Information("Exiting application");
+  //     Log.CloseAndFlush(); //To close our logger resource
+  //     repeat = false;
+  //     break;
+  //   default:
+  //     Console.WriteLine("Page does not exist!");
+  //     Console.WriteLine("Please press Enter to continue");
+  //     Console.ReadLine();
+  //     break;
+  // }
+
 }
